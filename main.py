cd5f3433652d5b074c4e05b102d1195f8758deec
Aditya Sureshram <adityasureshram@Adityas-MacBook-Air.local>
2023-10-29 12:06:05 +0530

    first commit

--- main.py
+++ main.py
@@ -1,24 +1,9 @@
 import os
-from dotenv import load_dotenv
-import pymongo
 import streamlit as st
 import torch
 import easyocr
-from PIL import Image
 import numpy as np
-import cv2
 from io import BytesIO,StringIO
-
-load_dotenv()
-
-def initConn():
-    #helper function to connect to mongodb
-    murl = os.getenv('MURL')
-    mclient = pymongo.MongoClient(murl)
-    mdb = 'sample_analytics'
-    conn = mclient[mdb]
-    return conn
-
 
 def extractTextFromImg(img):
     reader = easyocr.Reader(['en', 'en'])
@@ -26,9 +11,3 @@ def extractTextFromImg(img):
     img_text = reader.readtext(img)
     text = ' '.join([x[1] for x in img_text])
     return text
-
-
-def load_image(img):
-    im = Image.open(img)
-    image = np.array(im)
-    return image
